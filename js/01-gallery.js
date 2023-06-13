69f1618371f230cdbbef8466e7b92a479897d528
mikus151 <mikus151@wp.pl>
2023-06-14 00:05:40 +0200

    summ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galeryItem = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galeryItem);
+
+gallery.addEventListener("click", openModal);
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}">
+    </div>
+`);
+
+  instance.show();
+};
