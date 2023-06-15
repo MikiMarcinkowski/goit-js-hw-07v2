e709a466f5706cd5e2aa306155dc7bcbb5897ca0
mikus151 <mikus151@wp.pl>
2023-06-16 00:15:38 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galeryItem = galleryItems
+  .map(
+    (item) =>
+      `
+      <li>
+        <a class="gallery__item" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>
+    
+      `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galeryItem);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  
+});
+
